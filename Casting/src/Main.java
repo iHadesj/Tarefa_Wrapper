03adc4ad72ba5492dfe3a15d30dc0613ae4d3053
edu <126445500+iHadesj@users.noreply.github.com>
2024-04-03 12:41:14 -0300

    Add files via upload

---
+++ Casting/src/Main.java
@@ -0,0 +1,12 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 100;
+        //declarando casting explicito
+        Integer num2;
+        num2 = (Integer) num1;
+
+        System.out.println("varivel int convertida explicitamente para tipo wrapper: " + num2);
+    }
+}
